890109d7df29bec45981b4d3e3d2cf6ac5856839
debasish2011 <92264640+debasish2011@users.noreply.github.com>
2022-07-23 23:24:40 +0530

    ok

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+
   title: {
     type: String,
     required: true,
@@ -11,6 +17,10 @@ const NotesSchema = new Schema({
   tag: {
     type: String,
     default: "General",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
   },
 });
 
