078c8ce3b24367cd3037ceed43abe1ab375190d1
debasish2011 <92264640+debasish2011@users.noreply.github.com>
2022-07-23 20:50:24 +0530

    ok

---
+++ Backend/middleware/fetchUser.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Mynameisdebasish";
+const fetchUser = (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ error: "Authentication Error!!!" });
+  }
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    res.status(401).send({ error: "Authentication Error!!!" });
+  }
+};
+
+module.exports = fetchUser;
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const fetchUser = require("../middleware/fetchUser");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/User");
 const router = express.Router();
+
+const JWT_SECRET = "Mynameisdebasish";
 
 router.post(
   "/createuser",
@@ -31,12 +35,67 @@ router.post(
         email: req.body.email,
         password: securedPassword,
       });
-      res.json(user);
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Error!!!");
     }
   }
 );
+
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please enter valid credentials." });
+      }
+      const comparePassword = await bcrypt.compare(password, user.password);
+      if (!comparePassword) {
+        return res
+          .status(400)
+          .json({ error: "Please enter valid credentials." });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Error!!!");
+    }
+  }
+);
+
+router.post("/getuser", fetchUser, async (req, res) => {
+  try {
+    const userID = req.user.id;
+    const user = await User.findById(userID).select("-password")
+    res.send(user)
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Error!!!");
+  }
+});
 
 module.exports = router;
